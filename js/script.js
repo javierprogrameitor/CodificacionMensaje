e097a2dc146e05b358a991e02c673c03c481ccd7
javierprogrameitor <javierprogrameitor@gmail.com>
2024-05-23 13:56:59 +0200

    change conflicto
    Ocurren conflictos con el merge

--- js/script.js
+++ js/script.js
@@ -1,52 +1,42 @@
-let miBoton = document.getElementById("miBoton");
-<<<<<<< HEAD
-let imagen1 = document.getElementById("img");
-let imagen2 = document.getElementById("img2");
-let boton = document.getElementById("boton");
-let alternar = false; // Inicialmente, imagen1 está visible
-=======
 let imagen1 = document.getElementById("img1");
 let imagen2 = document.getElementById("img2");
 let boton = document.getElementById("boton");
+let miBoton = document.getElementById("miBoton");
 let alternar = true; // Inicialmente, imagen1 está visible
->>>>>>> a5615a270a09f63c7ab2f4e5a2d2c22b8bc88073
 
-miBoton.addEventListener("click", function (){
-    let entradaUsuario = prompt("Introduce una frase?");
-    desplazarCaracteres(entradaUsuario);
-}); 
+miBoton.addEventListener("click", function() {
+    let entradaUsuario = prompt("Introduce una frase");
+    if (entradaUsuario) {
+        desplazarCaracteres(entradaUsuario);
+    }
+});
 
- function desplazarCaracteres(frase){
-
+function desplazarCaracteres(frase) {
     let caracteres = frase.split('');
 
-    for(let i = 0; i < caracteres.length; ++i) {
-        let diferencia = caracteres[i].charCodeAt() - 'A'.charCodeAt();
-        diferencia = (diferencia + 1) % 26;
+    for (let i = 0; i < caracteres.length; ++i) {
+        let codigo = caracteres[i].charCodeAt();
 
-        let numero = caracteres[i].charCodeAt();
-        if(!isNaN(numero)){
-            let numeroSiguiente = numero +1;
-            caracteres[i] = numeroSiguiente;
+        // Desplazar solo letras mayúsculas y minúsculas
+        if (codigo >= 65 && codigo <= 90) { // Letras mayúsculas
+            caracteres[i] = String.fromCharCode((codigo - 65 + 1) % 26 + 65);
+        } else if (codigo >= 97 && codigo <= 122) { // Letras minúsculas
+            caracteres[i] = String.fromCharCode((codigo - 97 + 1) % 26 + 97);
+        } else if (codigo >= 48 && codigo <= 57) { // Números
+            caracteres[i] = String.fromCharCode((codigo - 48 + 1) % 10 + 48);
         }
-        caracteres[i] = String.fromCharCode(diferencia + 'A'.charCodeAt());
-        
     }
 
     alert(caracteres.join(''));
-};
+}
 
-boton.addEventListener("click", function () {
+boton.addEventListener("click", function() {
     if (alternar) {
-        imagen1.style.display = "block";
-        imagen2.style.display = "none";
-    } else {
         imagen1.style.display = "none";
         imagen2.style.display = "block";
+    } else {
+        imagen1.style.display = "block";
+        imagen2.style.display = "none";
     }
-    
     alternar = !alternar; // Cambiar el estado de alternar
 });
-
-
-
